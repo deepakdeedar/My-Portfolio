79923617c71cb66a13f90e32a259728669fc33b0
Deepak Deedar Singla <47693198+deepakdeedar@users.noreply.github.com>
2020-09-23 23:00:34 +0530

    create database

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+import csv
+
 app = Flask(__name__)
 
 
@@ -10,3 +12,23 @@ def hello_world():
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database, delimiter=",",
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'Something went wrong. Please try again later.'
