305d28a8b8b842836c989cb2d44fed09ead30d2d
Deepak Deedar Singla <47693198+deepakdeedar@users.noreply.github.com>
2020-09-23 22:10:04 +0530

    first commit

--- server.py
+++ server.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/<username>')
-def hello_world(username=None):
-    return render_template('index.html', name=username)
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
